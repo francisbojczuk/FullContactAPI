eef482fa54bd7fdb5cb5b797f3c517152a4c551d
root <annamakarovaupwork@hotmail.com>
2019-06-12 16:18:42 +0800

    scraper update

--- app/__init__.py
+++ app/__init__.py
@@ -17,84 +17,86 @@ def create_app(config_name):
     	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
     ###########################################
-    column_names = ['email','twitter','phone','fullName','ageRange','gender','location','title','organization',
-                    'linkedin','facebook','bio','avatar','website','employment_name','employment_domain','employment_status',
-                    'employment_title','employment_start','employment_end','interests_name','interests_id',
-                    'interests_affinity','interests_parrentIds','interests_category','profile_twitter_info',
-                    'photos_label','photos_value','urls_lable','urls_value','name_given','name_family','name_middle','name_prefix',
-                    'name_suffix','name_nickname','birthday', 'age_range','age_value','gender','location_label',
-                    'location_city','location_region','location_reg_code','location_country','location_coutry_code','location_formatted',
-                    'education_name','education_degree','education_end','email_md5','email_sha256','topics','keypeople',
-                    'dataAddOns','updated']
+    column_names = ['email','fullName','ageRange','gender','location','title','organization','twitter','linkedin',
+                    'facebook','bio','avatar','website', 'name_given','name_family','name_middle','name_prefix',
+                    'name_suffix','name_nickname', 'birthday', 'age_range','age_value', 'gender', 'phones','profiles' ,'employment_name',
+                    'employment_domain','employment_status','employment_title','employment_start','employment_end', 
+                    'photos_label','photos_value', 'education_name','education_degree','education_end','urls_label','urls_value',
+                    'interests_name','interests_id','interests_affinity','interests_parentIds','interests_category', 'topics',
+                    'keypeople_name', 'keypeople_title', 'keypeople_avatar','addon_id', 'addon_name', 'addon_enabled', 'addon_applied',
+                    'addon_description', 'addon_docLink','updated']
+                    
     def get_csv_row(x,email):
         ret = []
         for column in column_names:
             ret.append(x.get(column, ''))
-        try:
-            ret[0] = email
-            if x.get('details'):
-                if x.get('details').get('employment'):
-                    ret[14] = x.get('details').get('employment').get('name','')
-                    ret[15] = x.get('details').get('employment').get('domain','')
-                    ret[16] = x.get('details').get('employment').get('current','')
-                    ret[17] = x.get('details').get('employment').get('title','')
-                    ret[18] = x.get('details').get('employment').get('start','')
-                    ret[19] = x.get('details').get('employment').get('end','')
-                if x.get('details').get('interests'):
-                    ret[20] = x.get('details').get('interests').get('name','')
-                    ret[21] = x.get('details').get('interests').get('id','')
-                    ret[22] = x.get('details').get('interests').get('affinity','')
-                    ret[23] = x.get('details').get('interests').get('parentIds','')
-                    ret[24] = x.get('details').get('interests').get('category','')
-                if x.get('details').get('profile'):
-                    ret[25] = x.get('details').get('profile').get('twitter','')
-                if x.get('details').get('photos'):
-                    ret[26] = x.get('details').get('photos').get('label','')
-                    ret[27] = x.get('details').get('photos').get('value','')
-                if x.get('details').get('urls'):
-                    ret[28] = x.get('details').get('urls').get('label','')
-                    ret[29] = x.get('details').get('urls').get('value','')
+    
+        ret[0] = email
+        if x.get('details'):
+            try:
                 if x.get('details').get('name'):
-                    ret[30] = x.get('details').get('name').get('given','')
-                    ret[31] = x.get('details').get('name').get('family','')
-                    ret[32] = x.get('details').get('name').get('middle','')
-                    ret[33] = x.get('details').get('name').get('prefix','')
-                    ret[34] = x.get('details').get('name').get('suffix','')
-                    ret[35] = x.get('details').get('name').get('nickname','')
+                    ret[13] = x.get('details').get('name').get('given','')
+                    ret[14] = x.get('details').get('name').get('family','')
+                    ret[15] = x.get('details').get('name').get('middle','')
+                    ret[16] = x.get('details').get('name').get('prefix','')
+                    ret[17] = x.get('details').get('name').get('suffix','')
+                    ret[18] = x.get('details').get('name').get('nickname','')
                 if x.get('details').get('age'):
-                    ret[36] = x.get('details').get('age').get('birthday','')
-                    ret[37] = x.get('details').get('age').get('range','')
-                    ret[38] = x.get('details').get('age').get('value','')
+                    ret[19] = x.get('details').get('age').get('birthday','')
+                    ret[20] = x.get('details').get('age').get('range','')
+                    ret[21] = x.get('details').get('age').get('value','')
                 if x.get('details').get('gender'):
-                    ret[39] = x.get('details').get('gender','')
-                if x.get('details').get('locations'):
-                    ret[40] = x.get('details').get('location').get('label','')
-                    ret[41] = x.get('details').get('location').get('city','')
-                    ret[42] = x.get('details').get('location').get('region','')
-                    ret[43] = x.get('details').get('location').get('regionCode','')
-                    ret[44] = x.get('details').get('location').get('country','')
-                    ret[45] = x.get('details').get('location').get('countryCode','')
-                    ret[46] = x.get('details').get('location').get('formatted','')
+                    ret[22] = x.get('details').get('gender','')
+                
+                ret[23] = x.get('details').get('phones','')
+                ret[24] = x.get('details').get('profiles','')
+                                    
+                if x.get('details').get('employment'):
+                    ret[25] = x.get('details').get('employment').get('name','')
+                    ret[26] = x.get('details').get('employment').get('domain','')
+                    ret[27] = x.get('details').get('employment').get('current','')
+                    ret[28] = x.get('details').get('employment').get('title','')
+                    ret[29] = x.get('details').get('employment').get('start','')
+                    ret[30] = x.get('details').get('employment').get('end','')
+                if x.get('details').get('photos'):
+                    ret[31] = x.get('details').get('photos').get('label','')
+                    ret[32] = x.get('details').get('photos').get('value','')
                 if x.get('details').get('education'):
-                    ret[47] = x.get('details').get('location').get('name','')
-                    ret[48] = x.get('details').get('location').get('degree','')
-                    ret[49] = x.get('details').get('location').get('end','')
-                if x.get('details').get('emails'):
-                    ret[50] = x.get('details').get('emails').get('value','')
-                    ret[51] = x.get('details').get('emails').get('md5','')
-                    ret[52] = x.get('details').get('emails').get('sha256','')
-                if x.get('details').get('topics'):
-                    ret[53] = x.get('details').get('topics','')
+                    for education in x.get('details').get('education'):
+                        ret[33] = ret[33] + ' | ' + str(education.get('name',''))
+                        ret[34] = ret[34] + ' | ' + str(education.get('degree',''))
+                        ret[35] = ret[35] + ' | ' + str(education.get('end',''))
+                if x.get('details').get('urls'):
+                    ret[36] = x.get('details').get('urls').get('label','')
+                    ret[37] = x.get('details').get('urls').get('value','')
+
+                if x.get('details').get('interests'):
+                    ret[38] = x.get('details').get('interests').get('name','')
+                    ret[39] = x.get('details').get('interests').get('id','')
+                    ret[40] = x.get('details').get('interests').get('affinity','')
+                    ret[41] = x.get('details').get('interests').get('parentIds','')
+                    ret[42] = x.get('details').get('interests').get('category','')
+                ret[43] = x.get('details').get('topics','')
                 if x.get('details').get('keyPeople'):
-                    ret[54] = x.get('details').get('keyPeople','')
+                    for keypeople in x.get('details').get('keyPeople'):
+                        ret[44] =ret[44] + ' | ' + str(keypeople.get('fullName'))
+                        ret[45] =ret[45] + ' | ' + str(keypeople.get('title'))
+                        ret[46] =ret[46] + ' | ' + str(keypeople.get('avatar'))
+            except:
+                pass
 
-            res[55] = x.get('dataAddOns','')
-            res[56] = x.get('updated','')
-
-        except:
-            pass
-
-        print(ret)
+        if x.get('dataAddOns'):
+            for dataAdd in x.get('dataAddOns'):
+                try:
+                    ret[47] =ret[47] + ' | ' + str(dataAdd.get('id'))
+                    ret[48] =ret[48] + ' | ' + str(dataAdd.get('name'))
+                    ret[49] =ret[49] + ' | ' + str(dataAdd.get('enabled'))
+                    ret[50] =ret[50] + ' | ' + str(dataAdd.get('applied'))
+                    ret[51] =ret[51] + ' | ' + str(dataAdd.get('description'))
+                    ret[52] =ret[52] + ' | ' + str(dataAdd.get('docLink'))
+                except:
+                    pass
+        print(ret)    
         return ret
     ###########################################
 
